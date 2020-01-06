8b277d77f2981bd98cea51e3a566fc943de5ddd2
IvarKristinn <hallssonivarkristinn@gmail.com>
2020-01-06 17:07:48 +0000

    Heroku https, hopefully

--- gretaratorfrontend/app.js
+++ gretaratorfrontend/app.js
@@ -1,5 +1,5 @@
 const express = require("express");
-const http = require("http");
+const https = require("https");
 const path = require("path");
 
 var app = express();
@@ -9,5 +9,5 @@ app.use(express.static(path.join(__dirname, "build")));
 const port = process.env.PORT || "8080";
 app.set("port", port);
 
-const server = http.createServer(app);
+const server = https.createServer(app);
 server.listen(port, () => console.log(`Running on localhost:${port}`));

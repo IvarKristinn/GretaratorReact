88da646e31579e4f5822fc2a2bbb170e4c45830b
IvarKristinn <hallssonivarkristinn@gmail.com>
2020-01-06 17:40:51 +0000

    OG api link

--- gretaratorfrontend/src/apis/gretarnator.js
+++ gretaratorfrontend/src/apis/gretarnator.js
@@ -2,5 +2,5 @@ import axios from "axios";
 // Api call to Gretarator api
 // Getting the list of nicknames
 export default axios.create({
-  baseURL: "http://gretarator.com"
+  baseURL: "https://gretarnator-api.herokuapp.com"
 });
